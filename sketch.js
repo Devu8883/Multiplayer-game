59992f4792cf88ec2f09452154ceec96605c06b4
Devu8883 <75876723+Devu8883@users.noreply.github.com>
2021-06-04 12:59:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var database,form,game,player,playerCount;
+var gameState = 0;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+  
+  
+}
+
